87c371840dd1306e12a11211dd32363af4466bb2
ouvens <1025224452@qq.com>
2016-05-17 21:21:33 +0800

    add

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ const http = require('http');
 
 const koa = require('koa');
 const logger = require('koa-logger');
-const route = require('koa-route');
+const router = require('koa-router');
 const serve = require('koa-static');
 const stylus = require('koa-stylus');
 
-const routes = require('./routes');
+const routeMap = require('./routes');
 
 // Create koa app
 const app = koa();
@@ -19,15 +19,15 @@ app.use(stylus('./pages'));
 app.use(serve('./pages'));
 
 // 路由中间件
-app.use(route.get('/index', routes.indexPage));
- 
-app.use(route.get('/', routes.list));
-app.use(route.get('/todo/new', routes.add));
-app.use(route.get('/todo/:id', routes.show));
-app.use(route.get('/todo/delete/:id', routes.remove));
-app.use(route.get('/todo/edit/:id', routes.edit));
-app.use(route.post('/todo/create', routes.create));
-app.use(route.post('/todo/update', routes.update));
+app.use(router.get('/index', routeMap.indexPage));
+
+app.use(router.get('/', routeMap.list));
+app.use(router.get('/todo/new', routeMap.add));
+app.use(router.get('/todo/:id', routeMap.show));
+app.use(router.get('/todo/delete/:id', routeMap.remove));
+app.use(router.get('/todo/edit/:id', routeMap.edit));
+app.use(router.post('/todo/create', routeMap.create));
+app.use(router.post('/todo/update', routeMap.update));
 
 // 创建服务器监听
 http.createServer(app.callback()).listen(3000);
