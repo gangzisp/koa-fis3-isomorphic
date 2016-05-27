1a2fcc3e1438ac4fbcf49eb19ed3e47e7bd670f6
ouvens <1025224452@qq.com>
2016-05-27 18:29:41 +0800

    add

--- server/routes/api.js
+++ server/routes/api.js
@@ -13,36 +13,21 @@ const config = require('./common/config')
 
 const DB = require('../models/db.class');
 
+
 /**
- * 获取评论列表内容处理
- * @param {[type]} req           [description]
+ * 上报规范：
+ * {
+ *     id: '121',                   // 业务id
+ *     form: 'http://test.qq.com/', // 来源页面，或者是native页面
+ *     msg: 'stack@test.js',        // 错误信息
+ *     userAgent: 'android',        // 用户环境，平台，浏览器可以自动分析
+ *     ip: '192.158.2.1',           // 用户上报ip
+ *     uin: '15383747382',          // 用户id
+ *     
+ * }
  * @yield {[type]} [description]
  */
-const _getCommentList = function*() {
-
-    let ctx = this;
-    let uri = 'http://test.xiaodao360.cn/mobile/index/get_comment_list?' + util.string.json2str(ctx.query);
-
-    let result = yield coRequest({
-        url: uri,
-        method: 'get',
-        headers: config.oAuthHeader
-    });
-
-    try {
-        let response = result;
-        let viewName;
-        let data = JSON.parse(response.body);
-
-        ctx.body = data;
-
-    } catch (e) {
-        ctx.redict('/');
-    }
-};
-
 const _report = function*() {
-
     let report = new DB('report');
 
     let data = yield report.find({
@@ -71,7 +56,7 @@ const _report = function*() {
         ctx.redict('/');
     }
 };
+
 module.exports = {
-    getCommentList: _getCommentList,
     report: _report
 }
