3c0ec15cd108fdb40c1be54418d7ecff78bc0b40
ouvens <1025224452@qq.com>
2016-08-11 18:24:23 +0800

    add

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,18 +5,10 @@
  */
 
 const organization = require('../controller/organization');
-const api = require('../controller/api');
 
 const router = require('koa-router')();
 
-/**
- * 轻社团主页
- */
-
-router.get('/index/detail/id/:id/type/:type', organization.detailPage);
-router.get('/index/get_comment_list', api.getCommentList);
-
-router.get('/org-rank.html', organization.orgRank);
-router.get('/star-org.html', organization.starOrg);
+router.get('/rank_list.html', organization.orgRank);
+router.get('/rank_detail.html', organization.starOrg);
 
 module.exports = router;
